dd45a589aabbea9b2f886d392f821a8a51b2f577
thandon263 <ncubethando20@gmail.com>
2018-11-16 23:15:31 -0500

    Updating a few things on my app --- disable links working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
      unless current_user.is_a?(GuestUser) 
          link_to  "Log Out", destroy_user_session_path, method: :delete, class: style
       else
-         (link_to "Register", new_user_registration_path, class: style) + 
+        #  (link_to "Register", new_user_registration_path, class: style) + 
         " ".html_safe +
          (link_to "Login", new_user_session_path, class: style) 
      end 
